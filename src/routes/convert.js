bf9f0df19332ff3c67e0543941310376389c505a
André Pinto <andrepinto.agalijo@gmail.com>
2019-10-27 08:18:40 +0100

    backend code upgrade

--- src/routes/convert.js
+++ src/routes/convert.js
@@ -2,35 +2,48 @@ const express = require('express'),
       { getCurrencies, convertCurrency } = require('../utils/currencyData'),
       router  = new express.Router();
 // -------------------------------------------------------------------------- \\
+const chalk = require('chalk')
+      log   = console.log,
+      err = chalk.bold.red,
+      msg   = chalk.bold.blue,
+      warn  = chalk.bold.yellow;
+// -------------------------------------------------------------------------- \\
 
 router.get('/', async (req, res) => {
-    const data = await getCurrencies();
-    const currencies = Object.keys(data.rates);
-
-    res.render('index', {
-        currencyName: currencies
-    });
-});
-
-router.get('/currencies', (req, res) => {
-
+    try {
+        const data = await getCurrencies();
+        const currencies = Object.keys(data.rates);
+    
+        res.render('index', {
+            currencyName: currencies,
+            test: req.body.test
+        });    
+    } catch (e) {
+        log(err('Unable to Connect! ') + e);
+        res.status(500).send(e);
+    }
 });
 
 router.post('/currencies', async (req, res, next) => {
-    const currencyAmount = req.body.currencyAmount || null,
-          currencyFrom   = req.body.currencyFrom,
-          currencyTo     = req.body.currencyTo,
-          currencyResult = req.body.currencyResult || null;
-    // -------------------------------------------------------- \\
+    try {
+        const currencyAmount = req.body.currencyAmount || null,
+        currencyFrom   = req.body.currencyFrom,
+        currencyTo     = req.body.currencyTo,
+        currencyResult = req.body.currencyResult || null;
+        // -------------------------------------------------------- \\
 
-    console.log('CURRENCY AMOUNT: ' + currencyAmount);
-    console.log('CURRENCY FROM: ' + currencyFrom);
-    console.log('CURRENCY TO: ' + currencyTo);
-    console.log('CURRENCY RESULT: ' + currencyResult);
-    
-    var test = await convertCurrency(currencyAmount, currencyFrom, currencyTo, currencyResult);
-    res.redirect('/');
-    // res.send('works');
+        log(msg('CURRENCY AMOUNT: ') + currencyAmount);
+        log(msg('CURRENCY FROM: ') + currencyFrom);
+        log(msg('CURRENCY TO: ') + currencyTo);
+        log(msg('CURRENCY RESULT: ') + currencyResult);
+        
+        var test = await convertCurrency(currencyAmount, currencyFrom, currencyTo);
+
+        res.redirect('/');   
+    } catch (e) {
+        log(err('Unable to Connect: ') + e);
+        res.status(500).send(e);
+    }
 });
 
 module.exports = router;
--- src/utils/currencyData.js
+++ src/utils/currencyData.js
@@ -1,6 +1,12 @@
 const axios = require('axios'),
-      api   = 'http://data.fixer.io/api/latest?access_key=8b4617c5c7b56543342a965cc339c715&format=1',
-      convertURl = 'http://data.fixer.io/api/convert?access_key=8b4617c5c7b56543342a965cc339c715';
+      api   = 'http://data.fixer.io/api/latest?access_key=8b4617c5c7b56543342a965cc339c715&format=1';
+// -------------------------------------------------------------------------------------------------- \\
+const chalk = require('chalk')
+      log   = console.log,
+      err = chalk.bold.red,
+      msg   = chalk.bold.blue,
+      warn  = chalk.bold.yellow;
+// ------------------------------- \\
 
 const getCurrencies = () => {
     try {
@@ -9,13 +15,13 @@ const getCurrencies = () => {
             return res.data;
         });
     } catch (e) {
-        console.log(e);
+        log(e);
     }
 }
 
-const convertCurrency = async (currencyAmount, currencyFrom, currencyTo, currencyResult) => {
-    const data = await getCurrencies();
+const convertCurrency = async (currencyAmount, currencyFrom, currencyTo) => {
     try {
+        const data = await getCurrencies();
         const currencies = Object.keys(data.rates);
     
         const cFromCurrency = currencies.find(function(currencyName) {
@@ -28,18 +34,18 @@ const convertCurrency = async (currencyAmount, currencyFrom, currencyTo, currenc
     
         var value1 = data.rates[cFromCurrency];
         var value2 = data.rates[cToCurrency];
-        console.log('FROM RATE: ' + value1 + ' TO RATE: ' + value2);
+        log(warn('FROM RATE: ') + value1 + warn(' TO RATE: ') + value2);
     
         var conversion = currencyAmount * value2;
-        console.log('CONVERSION: ' + conversion);
+        log(warn('CONVERSION: ') + conversion);
     
         var conversion1 = value2/(currencyAmount*value1);
-        console.log(conversion1)
-        console.log('/-------------------------------------------/');
+        log(warn('FINAL RESULT: ') + conversion1);
+        log('/-------------------------------------------/');
 
         return conversion1;
     } catch (e) {
-        console.log(e);
+        log(err('Unable to Connect') + e);
     }
 }
 
