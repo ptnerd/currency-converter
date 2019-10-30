33f2283e773f6286ee77bd419b6fd4cb7dcd7d3b
André Pinto <andrepinto.agalijo@gmail.com>
2019-10-30 09:38:58 +0100

    syntax update

--- public/js/appScript.js
+++ public/js/appScript.js
@@ -5,8 +5,8 @@ const cFrom = document.getElementById('currencyFrom');
 cFrom.value = 'EUR';
 const cTo = document.getElementById('currencyTo');
 cTo.value = 'CZK';
-const messageOne = document.getElementById('message'); // First Input Box
-const messageTwo = document.getElementById('message-1'); // Second Input Box
+const inputOne = document.getElementById('message'); // First Input Box
+const inputTwo = document.getElementById('message-1'); // Second Input Box
 // ------------------------------------------------------------------------- \\
 
 currencyForm.addEventListener('submit', (e) => {
@@ -17,19 +17,19 @@ currencyForm.addEventListener('submit', (e) => {
     const currencyTo = cTo.value;
     const url = '/currencies?amount='+amount+'&currencyFrom='+currencyFrom+'&currencyTo='+currencyTo;
 
-    messageOne.textContent = 'Loading...';
+    inputOne.textContent = 'Loading...';
 
     fetch(url).then((response) => {
         response.json().then((data) => {
             if(data.error){
-                messageOne.textContent = data.error;
-                messageTwo.value = '';
+                inputOne.textContent = data.error;
+                inputTwo.value = '';
             } else if (data.result == null) {
-                messageOne.textContent = 'Please enter a valid number value';
-                messageTwo.value = '';
+                inputOne.textContent = 'Please enter a valid number value';
+                inputTwo.value = '';
             } else {
-                messageOne.textContent = ''; 
-                messageTwo.value = parseFloat(data.result).toFixed(3); 
+                inputOne.textContent = ''; 
+                inputTwo.value = parseFloat(data.result).toFixed(3); 
             }
         });
     });
